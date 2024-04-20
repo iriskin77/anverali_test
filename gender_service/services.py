bdb9d29051c4b57fc4c9cd0761497b8889d3cc8d
iriskin77 <talcore@yandex.ru>
2024-04-20 23:09:41 +0500

    fix: readme and checks in services

--- gender_service/services.py
+++ gender_service/services.py
@@ -7,13 +7,17 @@ def set_gender(contacts: ContactRequest) -> dict:
 
     man = MenNames.objects.filter(Q(id=contacts.id) & Q(names_man=contacts.name)).first()
     if man is not None:
-        Gender.objects.filter(id=man.id).update(gender="M")
+        gender = Gender.objects.get(gender="M")
+        man.gender = gender
+        man.save()
         return {"id": man.id, "name": man.names_man, "gender": "M"}
 
     woman = WomenNames.objects.filter(Q(id=contacts.id) & Q(names_woman=contacts.name)).first()
     if woman is not None:
-        Gender.objects.filter(id=woman.id).update(gender="F")
-        return {"id": woman.id, "name": woman.names_man, "gender": "F"}
+        gender = Gender.objects.get(gender="F")
+        woman.gender = gender
+        woman.save()
+        return {"id": woman.id, "name": woman.names_woman, "gender": "F"}
 
 
 
